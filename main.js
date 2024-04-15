c3147c0f6794003d4e835a76e0a3aac6b4dbf8c4
Vansh Dhalor <129588751+vansh-frontend@users.noreply.github.com>
2024-04-15 10:16:41 +0530

    Update main.js

--- main.js
+++ main.js
@@ -39,11 +39,12 @@ function handleKeyPress(event) {
 }
 
 // search in mobile input
-function searchOnGoogle() {
+function searchOnGooggle() {
   var searchQuery = document.getElementById('searchInputt').value;
   var searchURL ='https://www.google.com/search?q='+searchQuery;
   window.open(searchURL,'_blank');
 }
+
 
 // image open js
 function showFullscreen(img) {
