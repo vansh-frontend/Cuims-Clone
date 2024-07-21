fe48aeb43756ee3429c27dda5958ce1a08956cde
vansh-frontend <vanshdhalor04>
2024-07-21 13:25:04 +0530

    initial

--- main.js
+++ main.js
@@ -21,6 +21,8 @@ window.onload = function() {
 
 
 
+
+
 // search like google
 function handleKeyPress(event, device) {
   // Check if Enter key is pressed
@@ -40,9 +42,6 @@ function handleKeyPress(event, device) {
     window.open(searchURL, '_blank');
   }
 }
-// ends here search google
-
-
 
 
 // image open js
